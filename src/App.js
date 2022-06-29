a206d844c55bad97b1271a9841b8e9e0e581c7de
Alex <30435251+AlPeFe@users.noreply.github.com>
2022-06-29 16:09:48 +0200

    g

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-
 import "./App.css";
 import MeComponent from "./components/Me/MeComponent";
 import Bio from "./components/Biography/bio";
+import React, { useEffect } from "react";
 import About from "./components/About/About";
+import data from "./data/data";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
-    <div className="min-h-screen px-3 px-5 bg-gray-100">
+    <div className="min-h-screen px-3 py-10 bg-gray-100">
       <div data-aos="fade-down" data-aos-duration="800">
         <MeComponent />
         <About></About>
-        <Bio></Bio>
-
+        <Skills skills={data.skills}></Skills>
       </div>
       <div
         data-aos="fade-up"
